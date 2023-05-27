b20789df89a65021e42f0371c8d27ecc73942662
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2023-05-27 20:51:31 +0500

    updated next config

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const isUserLoggedIn = false;
   const [providers, setProviders] = useState(null);
   const [toggle, setToggle] = useState(false);
   useEffect(() => {

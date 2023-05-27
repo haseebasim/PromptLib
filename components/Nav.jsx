5aea32a887041aa1a8df54fca4c009e22853722b
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2023-05-27 21:33:43 +0500

    fixed auth api route and used useSession hook in nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
   const [providers, setProviders] = useState(null);
   const [toggle, setToggle] = useState(false);
   useEffect(() => {
@@ -15,6 +15,7 @@ const Nav = () => {
       setProviders(response);
     })();
   });
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href={"/"} className="flex gap-2 flex-center">
@@ -29,7 +30,7 @@ const Nav = () => {
       </Link>
       {/* Desktop Nav */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href={"/create-prompt"} className="black_btn">
               Create Post
@@ -39,7 +40,7 @@ const Nav = () => {
             </button>
             <Link href={"/profile"}>
               <Image
-                src={"/assets/images/logo.svg"}
+                src={session?.user.image}
                 width={37}
                 height={37}
                 alt="profile"
@@ -54,10 +55,10 @@ const Nav = () => {
 
       {/* Mobile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src={"/assets/images/logo.svg"}
+              src={session?.user.image}
               width={37}
               height={37}
               alt="profile"

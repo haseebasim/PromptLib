eca34cf4c5b6feac7c932765c7f071859e8c3c9f
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2023-05-27 20:37:57 +0500

    added user model and logic for signin

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import { signIn } from "next-auth/react";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -12,8 +13,23 @@ const handler = NextAuth({
   async session({ session }) {},
   async signIn({ profile }) {
     try {
-         
-    } catch (error) {}
+      await connectToDB();
+
+      const userExists = await User.findOne({ email: profile.email });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   },
 });
 
